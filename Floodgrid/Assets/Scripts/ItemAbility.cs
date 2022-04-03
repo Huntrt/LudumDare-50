f28f3751f1462874603379ae6e4b468e6d38705c
Huntrt <huntrt.business@outlook.com>
2022-04-03 20:35:30 +0700

    Complete freeze ray and run indicator

--- Floodgrid/Assets/Scripts/ItemAbility.cs
+++ Floodgrid/Assets/Scripts/ItemAbility.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class ItemAbility : MonoBehaviour
 {
 	Player p; Map m; void Awake() {p = Player.i; m = Map.i;}
-	public Item.Type useAbility; GameObject tempIndicator; Node tempNode;
-	List<Node> affectsNode = new List<Node>(); Sprite[] defaultSprite;
+	public Item.Type useAbility; 
+	GameObject tempIndicator; List<GameObject> tempIndicatorList = new List<GameObject>();
+	Node tempNode;
+	List<Node> affectsNode = new List<Node>();
 	[Header("Run")]
 	public int runDistance;
 	public GameObject runIndicator;
@@ -13,6 +15,7 @@ public class ItemAbility : MonoBehaviour
 	public GameObject bombIndicator;
 	[Header("Freeze Ray")]
 	public int freezeRayDuration;
+	public GameObject freezeRayIndicator;
 
     public void UseAbility(Item.Type use)
     {
@@ -40,11 +43,31 @@ public class ItemAbility : MonoBehaviour
 			p.speed = 1;
 			//No longer use any ability
 			useAbility = Item.Type.none;
+			ClearList();
 			//Complete casting of this ability
 			CompleteCast(); return;
 		}
-		//Increase player speed by 4
-		p.speed = 4;
+		ClearList();
+		//Get the starting node under player
+		Node starting = m.FindNodeAtCoordinates(p.coordinates);
+		//For each of run distance in all direction
+		for (int r = 1; r < runDistance+1; r++) for (int a = 0; a < 4; a++)
+		{
+			//Get the node at starting coordinates outward up to distance in 4 direction
+			Node n = m.FindNodeAtCoordinates(starting.coord + m.DirectionVector(a, r));
+			//If the node exist
+			if(n != null)
+			{
+				//Create run indicator at this node
+				GameObject indi = Instantiate(runIndicator, n.position, Quaternion.identity);
+				//Add indicator to it temp list
+				tempIndicatorList.Add(indi);
+				//Make the indicator look forward at current direction
+				indi.transform.up = (indi.transform.position + (Vector3)m.DirectionVector(a,1)) - indi.transform.position;
+			}
+		}
+		//Increase player speed by run distance
+		p.speed = runDistance;
 		//Are now use run ability
 		useAbility = Item.Type.run;
 	}
@@ -92,13 +115,13 @@ public class ItemAbility : MonoBehaviour
 
 	void FreezeRay(Vector2 fireDirection)
 	{
+		//Clear affecting node
+		ClearList();
+		//Get the starting node at player
+		Node starting = Map.i.FindNodeAtCoordinates(p.coordinates);
 		//If are using freeze ray ability with an fire direction
 		if(useAbility == Item.Type.freezeRay && fireDirection != Vector2.zero)
 		{
-			//Clear affecting node
-			ClearAffect();
-			//Get the starting node at player
-			Node starting = Map.i.FindNodeAtCoordinates(p.coordinates);
 			//Add the first affect node at starting
 			affectsNode.Add(starting);
 			//For all the node at direction
@@ -120,9 +143,29 @@ public class ItemAbility : MonoBehaviour
 			//Movement is no longer lock
 			p.lockMovement = false;
 			//Clear affecting node
-			ClearAffect();
+			ClearList();
 			//Complete casting of this ability
 			CompleteCast(); return;
+		}
+		//For each all of distance in this direction
+		for (int r = 1; r < 100; r++) for (int a = 0; a < 4; a++)
+		{
+			//Get the node at starting coordinates outward up to distance in 4 direction
+			Node n = m.FindNodeAtCoordinates(starting.coord + m.DirectionVector(a, r));
+			//If the node exist
+			if(n != null)
+			{
+				//And is flooded
+				if(n.flooded)
+				{
+					//Create run indicator at this node
+					GameObject indi = Instantiate(freezeRayIndicator, n.position, Quaternion.identity);
+					//Add indicator to it temp list
+					tempIndicatorList.Add(indi);
+				}
+			}
+			//Stop create indicator if no more node left
+			else {break;}
 		}
 		//Now using freeze ray ability
 		useAbility = Item.Type.freezeRay;
@@ -138,7 +181,12 @@ public class ItemAbility : MonoBehaviour
 		p.ClearItem();
 	}
 
-	void ClearAffect() {affectsNode.Clear(); affectsNode = new List<Node>();}
+	void ClearList() 
+	{
+		for (int t = 0; t < tempIndicatorList.Count; t++) {Destroy(tempIndicatorList[t]);}
+		affectsNode.Clear(); affectsNode = new List<Node>();
+		tempIndicatorList.Clear(); tempIndicatorList = new List<GameObject>();
+	}
 
 	public void ClearAbility()
 	{
